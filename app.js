6e79044de4fda338496e20298e41c9f3a9bb7ff9
Hammad Afzal <hammad.afzal.code@gmail.com>
2024-07-28 22:03:43 +0500

    code till user model creation

---
+++ app.js
@@ -0,0 +1,27 @@
+require("dotenv").config({ path: `${process.cwd()}/.env` });
+const express = require("express");
+
+const authRouter = require("./route/authRoute");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Hey there",
+  });
+});
+// all routes
+
+app.use("/api/v1/auth", authRouter);
+
+app.use("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: "route not found",
+  });
+});
+app.listen(PORT, () => {
+  console.log("server is listening on port:", PORT);
+});
